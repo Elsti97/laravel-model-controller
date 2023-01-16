abd2a775c64f271fd8364ea272449e1e3ae5f1f3
Elsti97 <elsti.shehati@hotmail.it>
2023-01-16 15:44:53 +0100

    Inseriti titoli film

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index(){
+
+        $all_movies = Movie::all();
+
+        return view('home', compact('all_movies') );
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+
+@section('main')
+
+    <h1>All Movies</h1>
+    @foreach ($all_movies as $elem )
+        <h4>{{$elem->title}}</h4>
+    @endforeach
+
+@endsection
